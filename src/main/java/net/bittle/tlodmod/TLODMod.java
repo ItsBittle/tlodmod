819c35362e58d03c8bfbb615d34bf84a97683911
Tyler Hansen <itsbittle@gmail.com>
2023-01-29 14:03:30 -0500

    Initial commit

---
+++ src/main/java/net/bittle/tlodmod/TLODMod.java
@@ -0,0 +1,15 @@
+package net.bittle.tlodmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TLODMod implements ModInitializer {
+	public static final String MOD_ID = "tlodmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bittle/tlodmod/TLODModClient.java
@@ -0,0 +1,10 @@
+package net.bittle.tlodmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TLODModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
